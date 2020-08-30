a6cae2cee662107bdf7d85243e6ce7291fe3ed61
exuanbo <exuanbo@protonmail.com>
2020-08-30 16:06:47 +0800

    chore: change project structure

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,18 @@ const pkg = require('./package.json')
 const commonjs = require('@rollup/plugin-commonjs')
 
 export default {
-  input: 'index.js',
+  input: 'src/index.js',
   plugins: [commonjs()],
   external: ['path', ...Object.keys(pkg.dependencies)],
-  output: {
-    file: pkg.module,
-    format: 'es'
-  }
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'auto'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    }
+  ]
 }
